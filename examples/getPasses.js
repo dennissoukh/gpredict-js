9fc8922f77622611b1fb825de26b192a23afa938
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2021-01-02 14:20:27 +0000

    format getPasses

--- examples/getPasses.js
+++ examples/getPasses.js
@@ -4,15 +4,21 @@ const PredictTLE    = require('../src/TLE');
 const PredictTime   = require('../src/Time');
 const PredictSat    = require('../src/Sat');
 
-predict = new Predict();
-qth     = new PredictQTH();
+predict     = new Predict();
+qth         = new PredictQTH();
 
-qth.name = 'Galway';
-qth.alt = 60;
-qth.lat = 53.28152;
-qth.lon = -8.999173;
+qth.name    = 'Galway';
+qth.alt     = 60;
+qth.lat     = 53.10;
+qth.lon     = -8.98;
 
-tle     = new PredictTLE('ISS (ZARYA)', '1 25544U 98067A   20366.36586220  .00001388  00000-0  33060-4 0  9995', '2 25544  51.6465  92.8260 0001147 168.5597 306.8389 15.49242490262580');
+const satTle  = {
+    line0: 'ISS (ZARYA)',
+    line1: '1 25544U 98067A   20366.36586220  .00001388  00000-0  33060-4 0  9995',
+    line2: '2 25544  51.6465  92.8260 0001147 168.5597 306.8389 15.49242490262580'
+}
+
+tle = new PredictTLE(satTle.line0, satTle.line1, satTle.line2);
 
 let sat = new PredictSat(tle),
     now = PredictTime.GetCurrentJulianTimestamp();
